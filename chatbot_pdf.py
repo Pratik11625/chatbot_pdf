659eaa7e71183142977a9d0b47ab81bec2fd08f2
PRATIK DIYEWAR <87273518+Pratik11625@users.noreply.github.com>
2024-10-09 22:18:12 +0530

    chatbot_pdf.py

--- chatbot_pdf.py
+++ chatbot_pdf.py
@@ -4,7 +4,7 @@ from langchain.chains import create_history_aware_retriever
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 # from langchain_chroma import Chroma
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
